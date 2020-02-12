d3677ffec679fcdbaf0f9d8f1eddb18f707ac73e
vaibhav <vaibhavbabu3475@gail.com>
2020-02-12 22:44:33 +0530

    Map loaded vabhav

--- app/src/main/java/com/example/sang_chalo/Home.kt
+++ app/src/main/java/com/example/sang_chalo/Home.kt
@@ -1,12 +1,78 @@
 package com.example.sang_chalo
 
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class Home : AppCompatActivity() {
+   lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
+    lateinit var locationManager: LocationManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            googleMap.isMyLocationEnabled = true
+
+//            val location1 = LatLng(13.03,77.60)
+//            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+//            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+//
+//            val location2 = LatLng(9.89,78.11)
+//            googleMap.addMarker(MarkerOptions().position(location2).title("Madurai"))
+//
+//
+//            val location3 = LatLng(13.00,77.00)
+//            googleMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+
+
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    val currentloc = LatLng(location!!.latitude,location!!.longitude)
+
+                    googleMap.addMarker(MarkerOptions().position(currentloc).title("Current Location"))
+
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentloc,12f))
+
+
+                    var geocoder = Geocoder(this, Locale.getDefault())
+                    var addresses = geocoder.getFromLocation(location!!.latitude, location!!.longitude, 1)
+                    var address = ""
+                    if (addresses.get(0).getAddressLine(0) != null) {
+                        address = addresses.get(0)
+                            .getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    }
+
+                    var city = addresses.get(0).getLocality();
+                    var state = addresses.get(0).getAdminArea();
+                    var country = addresses.get(0).getCountryName();
+                    var postalCode = addresses.get(0).getPostalCode();
+                    var knownName = addresses.get(0).getFeatureName()
+
+                }
+
+
+        })
     }
+
 }
--- app/src/main/java/com/example/sang_chalo/MainActivity.kt
+++ app/src/main/java/com/example/sang_chalo/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.sang_chalo
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_ASK_PERMISSIONS = 1
+
+    private val RUNTIME_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.ACCESS_NETWORK_STATE)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestPermissions()
 
         val timer = object : Thread() {
             override fun run() {
@@ -42,4 +59,29 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun requestPermissions() {
+        if (!hasPermissions()) {
+            ActivityCompat.requestPermissions(this,
+                RUNTIME_PERMISSIONS, REQUEST_CODE_ASK_PERMISSIONS)
+        } else {
+
+        }
+    }
+    private fun String.permissionGranted(ctx: Context) =
+        ContextCompat.checkSelfPermission(ctx, this) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasPermissions(): Boolean {
+        /**
+         * Only when the app's target SDK is 23 or higher, it requests each dangerous permissions it
+         * needs when the app is running.
+         */
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true
+        }
+
+        return RUNTIME_PERMISSIONS.count { !it.permissionGranted(this) } == 0
+    }
+
+
 }
