fb1235425b5216a65b6375def311dff2fee8706d
keshavsharma24 <58415529+keshavsharma24@users.noreply.github.com>
2020-02-13 20:50:13 +0530

    keshv commit

---
+++ app/src/main/java/com/example/sang_chalo/Details.kt
@@ -0,0 +1,12 @@
+package com.example.sang_chalo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Details : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
